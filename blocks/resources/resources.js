6b03b701293224c6bb66b6514b210da1ecfefd8e
Amol Anand <amol.anand@gmail.com>
2023-06-21 13:39:45 -0400

    Resources with default Card functionality

---
+++ blocks/resources/resources.js
@@ -0,0 +1,45 @@
+import { lookupPages } from '../../scripts/scripts.js';
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+/**
+ * Creates a Card using a JSON object and style associated with the card
+ * @param {Object} row JSON Object typically coming from an index array item
+ * @param {Array} styles Class names that needs to be added to the card root div
+ */
+async function createCard(row, styles) {
+  // Create card div
+  const card = document.createElement('div');
+  if (styles) card.classList.add(styles);
+
+  // Add the image to the card first
+  if ((row.image !== '0') && (row.title !== '0')) {
+    const picture = createOptimizedPicture(row.image, row.title);
+    const picLink = document.createElement('a');
+    picLink.href = row.path;
+    if (picture) picLink.append(picture);
+    card.append(picLink);
+  }
+
+  // Create and add the link, title, author, readtime and category to card content and card
+  const link = document.createElement('a');
+  // link.classList.add('blog-link');
+  link.href = row.path;
+  if (row.title && row.title !== '0') link.innerHTML += `<h3>${row.title}</h3>`;
+  card.append(link);
+  if (row.description && row.description !== '0') card.innerHTML += `<div class="description"><p>${row.description}</p></div>`;
+  return (card);
+}
+
+export default async function decorate(block) {
+  const pathnames = [...block.querySelectorAll('a')].map((a) => new URL(a.href).pathname);
+  block.textContent = '';
+  // Make a call to the index and get the json for just the pathnames the author has put in
+  const pageList = await lookupPages(pathnames);
+  if (pageList.length) {
+    pageList.forEach(async (row) => {
+      block.append(await createCard(row, 'article-card'));
+    });
+  } else {
+    block.remove();
+  }
+}
