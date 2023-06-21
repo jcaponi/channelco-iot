e2e21de55a0cfd9d904799a47cb58d8332ad2915
Ravi Kiran Papani <rpapani@adobe.com>
2023-06-21 16:51:18 -0700

    feat: footer block

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,4 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
@@ -17,9 +17,29 @@ export default async function decorate(block) {
 
     // decorate footer DOM
     const footer = document.createElement('div');
-    footer.innerHTML = html;
-
-    decorateIcons(footer);
+    footer.innerHTML = `
+      <div class="footer-logo">
+        <img src="/icons/logo-thechannelco.png" alt="The Channel Company">
+      </div>
+      ${html}
+    `;
+    footer.classList.add('footer-logolinks');
+    const footerLogo = footer.querySelector('.footer-logo');
+    const footerLinks = footerLogo.nextElementSibling;
+    let footerAbout;
+    if (footerLinks) {
+      footerLinks.classList.add('footer-links');
+      footerAbout = footerLinks.nextElementSibling;
+      if (footerAbout) {
+        footerAbout.classList.add('footer-about');
+        footerAbout.firstElementChild.classList.add('footer-about-title');
+        footerAbout.lastElementChild.classList.add('footer-about-content');
+        block.append(footerAbout);
+      }
+    }
     block.append(footer);
+    if (footerAbout) {
+      block.append(footerAbout);
+    }
   }
 }
