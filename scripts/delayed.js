ac47b65801d7e8785b1c4b9ebfdbd505bfd9d636
Amol Anand <amol.anand@gmail.com>
2023-06-27 20:28:14 -0400

    Add top and aside Ad containers

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,6 +6,21 @@ import {
 } from './lib-franklin.js';
 
 // add more delayed functionality here
+function buildTopAd(main) {
+  const topAdHTML = `
+  <div class="top-ad">
+    <span class="ad-title">Advertisement</span> <br />
+    <!-- /21804213519/CRN/Ros_Top_Leader-->
+    <div id="unit-1659129517463" class="tmsads"></div>
+  </div>
+  `;
+  const range = document.createRange();
+  const topAdEl = range.createContextualFragment(topAdHTML);
+  const contentContainer = main.querySelector('.content-container');
+  if (contentContainer) {
+    contentContainer.parentNode.insertBefore(topAdEl, contentContainer);
+  }
+}
 
 function buildAsideBlock(main) {
   const block = buildBlock('aside', [[]]);
@@ -26,6 +41,7 @@ function buildAsideBlock(main) {
 function buildDelayedAutoBlocks(main) {
   try {
     buildAsideBlock(main);
+    buildTopAd(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Delayed Auto Blocking failed', error);
