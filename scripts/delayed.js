67b023c1847439fc9f36cb4fe1a77f4abe5c7d76
Amol Anand <amol.anand@gmail.com>
2023-06-28 15:52:58 -0400

    commented out funnelfuel and sourcepoint

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -59,7 +59,7 @@ function addMartechStack() {
     async: '',
     'data-tmsclient': 'CRN',
     'data-layout': 'ros',
-    'data-debug': 'true',
+    'data-debug': 'false',
   });
   const globalAdScript = 'window.TAS = window.TAS || { cmd: [] }';
   loadScript('', {}, globalAdScript);
@@ -88,11 +88,11 @@ function addMartechStack() {
   const spTag1 = `
     function _typeof(t){return(_typeof="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(t){return typeof t}:function(t){return t&&"function"==typeof Symbol&&t.constructor===Symbol&&t!==Symbol.prototype?"symbol":typeof t})(t)}!function(){for(var t,e,o=[],n=window,r=n;r;){try{if(r.frames.__tcfapiLocator){t=r;break}}catch(t){}if(r===n.top)break;r=n.parent}t||(function t(){var e=n.document,o=!!n.frames.__tcfapiLocator;if(!o)if(e.body){var r=e.createElement("iframe");r.style.cssText="display:none",r.name="__tcfapiLocator",e.body.appendChild(r)}else setTimeout(t,5);return!o}(),n.__tcfapi=function(){for(var t=arguments.length,n=new Array(t),r=0;r<t;r++)n[r]=arguments[r];if(!n.length)return o;"setGdprApplies"===n[0]?n.length>3&&2===parseInt(n[1],10)&&"boolean"==typeof n[3]&&(e=n[3],"function"==typeof n[2]&&n[2]("set",!0)):"ping"===n[0]?"function"==typeof n[2]&&n[2]({gdprApplies:e,cmpLoaded:!1,cmpStatus:"stub"}):o.push(n)},n.addEventListener("message",(function(t){var e="string"==typeof t.data,o={};if(e)try{o=JSON.parse(t.data)}catch(t){}else o=t.data;var n="object"===_typeof(o)?o.__tcfapiCall:null;n&&window.__tcfapi(n.command,n.version,(function(o,r){var a={__tcfapiReturn:{returnValue:o,success:r,callId:n.callId}};t&&t.source&&t.source.postMessage&&t.source.postMessage(e?JSON.stringify(a):a,"*")}),n.parameter)}),!1))}();
   `;
-  loadScript('', {}, spTag1);
+  // loadScript('', {}, spTag1);
   const spTag2 = `
     (function () { var e = false; var c = window; var t = document; function r() { if (!c.frames["__uspapiLocator"]) { if (t.body) { var a = t.body; var e = t.createElement("iframe"); e.style.cssText = "display:none"; e.name = "__uspapiLocator"; a.appendChild(e) } else { setTimeout(r, 5) } } } r(); function p() { var a = arguments; __uspapi.a = __uspapi.a || []; if (!a.length) { return __uspapi.a } else if (a[0] === "ping") { a[2]({ gdprAppliesGlobally: e, cmpLoaded: false }, true) } else { __uspapi.a.push([].slice.apply(a)) } } function l(t) { var r = typeof t.data === "string"; try { var a = r ? JSON.parse(t.data) : t.data; if (a.__cmpCall) { var n = a.__cmpCall; c.__uspapi(n.command, n.parameter, function (a, e) { var c = { __cmpReturn: { returnValue: a, success: e, callId: n.callId } }; t.source.postMessage(r ? JSON.stringify(c) : c, "*") }) } } catch (a) { } } if (typeof __uspapi !== "function") { c.__uspapi = p; __uspapi.msgHandler = l; c.addEventListener("message", l, false) } })();
   `;
-  loadScript('', {}, spTag2);
+  // loadScript('', {}, spTag2);
   const spTag3 = `
     window._sp_queue = [];
     window._sp_ = {
@@ -135,8 +135,8 @@ function addMartechStack() {
         }
     }
   `;
-  loadScript('', {}, spTag3);
-  loadScript('https://cdn.privacy-mgmt.com/unified/wrapperMessagingWithoutDetection.js', { async: '' });
+  // loadScript('', {}, spTag3);
+  // loadScript('https://cdn.privacy-mgmt.com/unified/wrapperMessagingWithoutDetection.js', { async: '' });
   // Add FunnelFuel Tag
   const funnelFuel = `
     var _paq = window._paq = window._paq || [];
@@ -151,7 +151,7 @@ function addMartechStack() {
       g.async=true; g.src=u+'js/tracker.php'; s.parentNode.insertBefore(g,s);
     })();
   `;
-  loadScript('', {}, funnelFuel);
+  // loadScript('', {}, funnelFuel);
 }
 
 /**
