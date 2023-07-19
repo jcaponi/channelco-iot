8e625abe33d8d8f891e76902f68af6cd05960f01
Ravi Kiran Papani <rpapani@adobe.com>
2023-07-19 14:26:14 -0700

    fix: using intersection observer for loading video

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -3,14 +3,21 @@ import { readBlockConfig } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
   const config = readBlockConfig(block);
   if (config.url) {
-    const html = `
-      <div class="video-container">
-        <div>
-            <iframe loading="lazy" allow="encrypted-media" allowfullscreen="" src="${config.url}"></iframe>
-        </div>
-      </div>
-    `;
-    block.innerHTML = html;
+    const observer = new IntersectionObserver((entries) => {
+      if (entries.some((e) => e.isIntersecting)) {
+        const html = `
+          <div class="video-container">
+            <div>
+                <iframe loading="lazy" allow="encrypted-media" allowfullscreen="" src="${config.url}"></iframe>
+            </div>
+          </div>
+        `;
+        block.innerHTML = html;
+        observer.disconnect();
+      }
+    });
+    observer.observe(block);
+    block.textContent = '';
   } else {
     block.remove();
   }
